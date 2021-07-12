bef9b6a8c6e0a943d9910446a2f54f9b1d006ceb
Eduardo Munari <munari.edu@gmail.com>
2021-07-11 21:42:58 -0300

    add code from lesson 04

---
+++ src/invoices/invoices.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, ValidationPipe } from '@nestjs/common';
+import { InvoicesService } from './invoices.service';
+import { KafkaCreateInvoiceDto } from './dto/create-invoice.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+@Controller('credit-cards/:creditCardNumber/invoices')
+export class InvoicesController {
+  constructor(private readonly invoicesService: InvoicesService) {}
+
+  @MessagePattern('transactions')
+  create(@Payload(new ValidationPipe()) message: KafkaCreateInvoiceDto) {
+    console.log('create');
+    return this.invoicesService.create(message.value);
+  }
+
+  @Get()
+  findAll(@Param('creditCardNumber') creditCardNumber: string) {
+    return this.invoicesService.findAll(creditCardNumber);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { EntityNotFoundExceptionFilter } from './exception-filters/entity-not-found.exception-filter';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: process.env.KAFKA_CLIENT_ID,
+        brokers: [process.env.KAFKA_HOST],
+      },
+      consumer: {
+        groupId: process.env.KAFKA_CONSUMER_GROUP_ID,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+}
+bootstrap();
